12cfdaf6c2055ec16da94901bc165144473c8230
the.....PiNk.....pAnTheR <uzairpachhapure@uzairs-MacBook-Pro.local>
2023-05-07 21:56:17 -0700

    fourth push

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^uy-t9*hj_i1=msi%o!8klek0mz7x_951@gyp@g%zn7s6b$!py"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sports-analysis.herokuapp.com']
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
